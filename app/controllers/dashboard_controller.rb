df8f300c474a34b8323f8ea34ec3c3ea79c381d4
Andy Hull <andyhull3@gmail.com>
2013-08-31 12:00:49 -0700

    reconstructed user workflow

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,28 +1,27 @@
 class DashboardController < ApplicationController
   def index
   	@user = current_user
-  	@sid = current_user.sid
-  	@sms = get_sms_from_twilio(@user)
+  	if current_user.sid
+	  	@sid = current_user.sid
+	  	@sms = get_sms_from_twilio(@user)
+	  end
   end
 
   def connect
-  	sid = params['AccountSid']
   	@user = current_user
-  	@user.sid = sid
-  	@user.save
+  	if params['AccountSid'] || @user.sid
+	  	sid = params['AccountSid']
+	  	@user.sid = sid
+	  	@user.save
+	  	redirect_to '/dashboard'
+	  end
 
-  	redirect_to '/dashboard'
-  end
-
-  def add_sid_to_twilio_connect_button(sid)
-  	
   end
 
   def get_sms_from_twilio(current_user)
 	  account_sid = current_user.sid
 		auth_token =  ENV["TWILIO_TOKEN"]
 		@client = Twilio::REST::Client.new account_sid, auth_token
-		 
 		# Loop over messages and print out a property for each one
 		@messages = @client.account.sms.messages
 
