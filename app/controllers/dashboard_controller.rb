315af564904afc3fd70cd73c55544a37b75e0fa4
Andy Hull <andyhull3@gmail.com>
2013-08-31 11:30:06 -0700

    removing pry call

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -22,7 +22,6 @@ class DashboardController < ApplicationController
 	  account_sid = current_user.sid
 		auth_token =  ENV["TWILIO_TOKEN"]
 		@client = Twilio::REST::Client.new account_sid, auth_token
-		binding.pry
 		 
 		# Loop over messages and print out a property for each one
 		@messages = @client.account.sms.messages
